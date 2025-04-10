62ae8587f25b8aca41f0ab8ee411a186b7c37ed2
yatinsingh2007 <kshtriyatinsingh@Kshtris-MacBook-Pro.local>
2025-04-11 02:27:31 +0530

    a small display fix in my signup component

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -17,15 +17,15 @@ const Signup = () => {
             <h1 className={darkMode ? 'text-4xl md:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 via-white to-indigo-300 mb-10' : 'text-4xl md:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 via-slate-800 to-yellow-500 mb-10'}>Create An Account in JEE-Vault and Start your remarkable Journey</h1>
             <div className='w-full md:w-1/2 lg:w-1/3 bg-gradient-to-br from-yellow-400 via-white to-indigo-300 p-8 rounded-xl shadow-lg shadow-yellow-300 mt-32 md:mt-40'>
                 <form>
-                    <div className='flex flex-col'>
+                    <div className='flex flex-col mb-2'>
                         <label className='text-sm mb-1 text-slate-900'>Email</label>
                         <input type='email' placeholder='Enter your email' required className='p-2 rounded-md bg-white text-white focus:outline-none focus:ring-2 focus:ring-purple-400'/>
                     </div>
-                    <div className='flex flex-col'>
+                    <div className='flex flex-col mb-2'>
                         <label className='text-sm mb-1 text-slate-900'>Password</label>
                         <input type='password' placeholder='Enter your password' required className='p-2 rounded-md bg-white text-white focus:outline-none focus:ring-2 focus:ring-purple-400'/>
                     </div>
-                    <div className='flex justify-between'>
+                    <div className='flex justify-between mb-2'>
                         <button type='submit' className='mt-4 p-2 rounded-lg bg-gradient-to-r from-yellow-400 to-yellow-600 text-white font-semibold shadow-md hover:scale-105 transition-transform duration-300'>
                             Sign Up
                         </button>
